6ad1ffc06c97eacb308b634f5bab2b2079986eba
Nick Stiegman <nick12@bu.edu>
2012-03-27 19:23:06 -0400

    JQuery changes

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,6 +8,7 @@ class LocationsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @locations }
+      format.json  { render :json => @locations }
     end
   end
 
