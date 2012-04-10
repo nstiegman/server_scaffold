f506fff5a5d2a97821ccdb9a28d81736fd450baf
Nick Stiegman <nick12@bu.edu>
2012-04-10 19:35:59 -0400

    New search

--- app/models/location.rb
+++ app/models/location.rb
@@ -40,7 +40,7 @@ class Location < ActiveRecord::Base
   def self.search(search)
 
     if search 
-      find(:all, :conditions => ['name LIKE?', "%#{search}%"])
+      find(:all, :conditions => ['name LIKE?', "%#{search.downcase}%"])
     else
       find(:all)
     end
