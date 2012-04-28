a2549580e4410b6dbd1ea7146005950ac01f999c
Nick Stiegman <nick12@bu.edu>
2012-04-28 17:58:00 -0400

    Location model finished

--- app/models/location.rb
+++ app/models/location.rb
@@ -43,13 +43,13 @@ class Location < ActiveRecord::Base
 #                    :uniqueness => { :case_sensitive => false }
 
   has_attached_file :photo,
-  :storage => :s3,
-  :s3_credentials => "#{Rails.root}/config/s3.yml",
   :styles => {
     :thumb  => "100x100",
     :medium => "200x200",
     :large => "600x400"
   },
+  :storage => :s3,
+  :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
   :path => ":attachment/:id/:style.:extension",
   :default_url => "/images/nomap_thumb.bmp"
     
