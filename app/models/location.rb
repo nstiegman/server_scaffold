f14c54f9bb92698c5c4aadda959e12e6a2a9b98b
Nick Stiegman <nick12@bu.edu>
2012-04-28 17:00:15 -0400

    Location model format

--- app/models/location.rb
+++ app/models/location.rb
@@ -16,16 +16,7 @@
 class Location < ActiveRecord::Base
   attr_accessible :latitude, :longitude, :map, :name, :photo, :photo_file_name, :photo_content_type, :photo_file_size, :photo_updated_at, :photo_url, 
    
-  has_attached_file :photo,
-    :styles => {
-    :thumb  => "100x100",
-    :medium => "200x200",
-    :large => "600x400"},
-  :storage => :s3,
-  :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-  :path => ":attachment/:id/:style.:extension",
-  :bucket => 'ece-bu'
-  #:default_url => "/images/nomap_thumb.bmp"
+  has_attached_file :photo, :styles => { :thumb  => "100x100", :medium => "200x200", :large => "600x400"}, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => ":attachment/:id/:style.:extension", :bucket => 'ece-bu', :default_url => "/images/nomap_thumb.bmp"
   
   
   validates_attachment_content_type :photo, :content_type => [ 'image/jpeg', 'image/png', 'image/gif', 'image/jpg', 'image/bmp'  ], :message => "is not an acceptable image file" 
